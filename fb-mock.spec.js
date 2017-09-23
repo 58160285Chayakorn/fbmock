5bee57f043ed5432a575d617e7012b55249461be
Chayakorn Chayapahniphatt <58160285go.buu.ac.th>
2017-09-23 15:45:58 +0700

    Test pass all expect

--- fb-mock.spec.js
+++ fb-mock.spec.js
@@ -1,6 +1,32 @@
+function myAuth(authService){
+
+    this.authService = authService
+    
+    this.signInWithFacebook = (phone, password) => {
+        let fbUserOject = this.authService(phone, password)
+        return fbUserOject
+       
+    }
+   
+    
+}
+
 test('Mock Function 1 should be return 1', () => {
     const returnInt = jest.fn(1).mockReturnValue(1)
     expect(returnInt(1)).toBe(1)
     expect(returnInt).toBeCalledWith(1)
 
+})
+
+
+test('Call sign in with facebook with valid credentail should pass', () => {
+    const facebookAuth = jest.fn('0817777788', 'abc12345')
+    .mockReturnValue({ name: 'Weera', facebookId: '1234567890', email: 'wkasetsin@gmail.com'})
+    let app = new myAuth(facebookAuth)
+    let phone = '0817777788'
+    let password = 'abc12345'
+    let value = app.signInWithFacebook(phone, password)
+    expect(value).toEqual({ name: 'Weera', facebookId: '1234567890', email: 'wkasetsin@gmail.com'})
+    expect(facebookAuth).toBeCalled()
+    expect(facebookAuth).toBeCalledWith('0817777788', 'abc12345')
 })
